1e0408d4b1e583b3fde5b2a1f08246057921e47b
Benedict Mendoza <mendozabenedict14@gmail.com>
2026-02-19 09:23:59 +0400

    Change XP,HP and power ups UI and add some animation

---
+++ Assets/Scripts/PowerUpButtonAnimator.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class PowerUpButtonAnimator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [Header("Animation Settings")]
+    public float hoverScale = 1.15f;
+    public float clickScale = 0.95f;
+    public float animDuration = 0.15f;
+    public Ease animEase = Ease.OutBack;
+
+    private Vector3 originalScale;
+    private bool isPointerDown = false;
+
+    void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!isPointerDown)
+        {
+            transform.DOScale(originalScale * hoverScale, animDuration).SetEase(animEase);
+                AudioManager.Instance.PlayPlayerShootSFX();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (!isPointerDown)
+            transform.DOScale(originalScale, animDuration).SetEase(animEase);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isPointerDown = true;
+        transform.DOScale(originalScale * clickScale, animDuration * 0.7f).SetEase(Ease.InOutQuad);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isPointerDown = false;
+        transform.DOScale(originalScale * hoverScale, animDuration).SetEase(animEase);
+    }
+}
