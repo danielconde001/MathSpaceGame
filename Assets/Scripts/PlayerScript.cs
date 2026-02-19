5dacd73c81802a599151d0be92ef263320d78d88
danielconde001 <danielconde001@yahoo.com>
2026-02-20 01:37:11 +0800

    vercel test

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,8 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private MeshRenderer playerMesh;
     [SerializeField] private AudioSource shootAudioSource;
     [SerializeField] private AudioSource spaceshipAmbienceAudioSource;
+    
+    [SerializeField] private GameObject test;
 
     private SpaceshipAttack attack;
     private SpaceshipMovement movement;
@@ -74,6 +76,10 @@ public class PlayerScript : MonoBehaviour
     private void Start()
     {
         AudioManager.Instance.PlaySpaceshipAmbienceSFX();
+        if (Application.isMobilePlatform)
+        {
+            test.SetActive(true);
+        }
     }
 
     public int GetDamage()
